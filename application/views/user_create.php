281a48313d49640032ceecb3107e5323127379ab
Leonardo Salinas <sal_chueco@hotmail.com>
2018-12-14 19:14:48 -0300

    Update user_create.php

--- application/views/user_create.php
+++ application/views/user_create.php
@@ -1,7 +1,7 @@
 <h2 class="h2-buscarAvanzado">Alta usuario:</h2>
 
 <section class="buscarAvanzadoContenedor">
-        <form action="/index/user/insert.php" method ="post" id="miform">
+        <form class="formCenter" action="/index/user/insert.php" method ="post" id="miform">
                 <input type="text" id="nombre" name="nombre" placeholder="Nombre..." maxlength="128" onblur="validarUsuario('nombre', this.value)">
                 <input type="text" id="apellido" name="apellido" placeholder="Apellido..." maxlength="128" onblur="validarUsuario('apellido', this.value)">
                 <input type="text" id="dni" name="dni" placeholder="DNI..." maxlength="128" onblur="validarUsuario('dni', this.value)">
